39052cea6f9984fe4ffbfe1c0021acc7ecfcc35e
andreanagy90 <84456821+andreanagy90@users.noreply.github.com>
2025-06-25 11:07:31 +0100

    Add files via upload
    change prime function, and 2048 bit support

--- create.py
+++ create.py
@@ -1,28 +1,28 @@
 import math
-from p_numbers import generate_prime
+#from p_numbers import generate_prime
+from Crypto.Util.number import getPrime, inverse        # import Crypto functions
 
 # generate public and private keys
 
-def keygen():
+def keygen(keysize= 2048):
 
 
 
-    while True:
-        p = generate_prime()
-        q = generate_prime()
-        if q !=p and p > 1000 and q > 1000:
-            break
+
+    p = getPrime(keysize//2)
+    q = getPrime(keysize//2)
+    while p == q:
+        q = getPrime(keysize//2)
+
     n = p * q
     z = (p -1) * (q-1)
 
     e = 65537
     
     if math.gcd(e, z) != 1:
-        e = 3
-        while math.gcd(e, z) != 1:
-            e += 2   
+        raise ValueError ("e and z no relative prime, generate again")
             
-    d = pow(e,-1,z)
+    d = inverse(e,z)
 
 
 
@@ -31,4 +31,6 @@ def keygen():
 
     with open ("private_key.txt","w") as priv:
         priv.write(f"{n}\n{d}")
+
+    return (n, e), (n, d)
 
